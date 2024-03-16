3963cc78ef777edab55301552e4ba273af8d52cf
Dyspersja <dyspersjamusic@gmail.com>
2024-03-16 16:01:57 +0100

    Added final variables for min and max board size values

--- src/main/java/com/dyspersja/Chessboard.java
+++ src/main/java/com/dyspersja/Chessboard.java
@@ -5,11 +5,16 @@ import lombok.Getter;
 @Getter
 public class Chessboard {
 
+    private static final int MIN_WIDTH = 6;
+    private static final int MAX_WIDTH = 70;
+    private static final int MIN_HEIGHT = 6;
+    private static final int MAX_HEIGHT = 40;
+
     private final int width;
     private final int height;
 
     public Chessboard(int width, int height) {
-        if( width < 6 || width > 70 || height < 6 || height > 40)
+        if( width < MIN_WIDTH || width > MAX_WIDTH || height < MIN_HEIGHT || height > MAX_HEIGHT)
             throw new IllegalArgumentException("Invalid Checkerboard size");
 
         this.width = width;
