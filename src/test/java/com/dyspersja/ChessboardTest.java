92f27ed8996612b9844b97da9704112855d8f555
Dyspersja <dyspersjamusic@gmail.com>
2024-03-16 14:51:25 +0100

    Changed multiple tests for a single parametrized test

--- src/test/java/com/dyspersja/ChessboardTest.java
+++ src/test/java/com/dyspersja/ChessboardTest.java
@@ -1,92 +1,31 @@
 package com.dyspersja;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ChessboardTest {
 
-    @Test
-    void shouldThrowExceptionWhenBoardsWidthIsNegativeNumber() {
-        // Given
-        int width = -2;
-        int height = 8;
+    @ParameterizedTest
+    @CsvSource({
+            "-2, 8",
+            "8, -3",
+            "2, 8",
+            "8, 5",
+            "75, 8",
+            "8, 45"
+    })
+    void shouldThrowExceptionWhenBoardsSizeIsInvalid(int width, int height) {
+        // Given ...
 
         // Then
         Throwable exception = assertThrows(IllegalArgumentException.class, () ->
                 // When
                 new Chessboard(width, height));
 
-        assertEquals("Wrong Checkerboard size", exception.getMessage());
+        assertEquals("Invalid Checkerboard size", exception.getMessage());
     }
 
-    @Test
-    void shouldThrowExceptionWhenBoardsHeightIsNegativeNumber() {
-        // Given
-        int width = 8;
-        int height = -3;
-
-        // Then
-        Throwable exception = assertThrows(IllegalArgumentException.class, () ->
-                // When
-                new Chessboard(width, height));
-
-        assertEquals("Wrong Checkerboard size", exception.getMessage());
-    }
-
-    @Test
-    void shouldThrowExceptionWhenBoardsWidthIsTooSmall() {
-        // Given
-        int width = 2;
-        int height = 8;
-
-        // Then
-        Throwable exception = assertThrows(IllegalArgumentException.class, () ->
-                // When
-                new Chessboard(width, height));
-
-        assertEquals("Wrong Checkerboard size", exception.getMessage());
-    }
-
-    @Test
-    void shouldThrowExceptionWhenBoardsHeightIsTooSmall() {
-        // Given
-        int width = 8;
-        int height = 5;
-
-        // Then
-        Throwable exception = assertThrows(IllegalArgumentException.class, () ->
-                // When
-                new Chessboard(width, height));
-
-        assertEquals("Wrong Checkerboard size", exception.getMessage());
-    }
-
-    @Test
-    void shouldThrowExceptionWhenBoardsWidthIsTooBig() {
-        // Given
-        int width = 75;
-        int height = 8;
-
-        // Then
-        Throwable exception = assertThrows(IllegalArgumentException.class, () ->
-                // When
-                new Chessboard(width, height));
-
-        assertEquals("Wrong Checkerboard size", exception.getMessage());
-    }
-
-    @Test
-    void shouldThrowExceptionWhenBoardsHeightIsTooBig() {
-        // Given
-        int width = 8;
-        int height = 45;
-
-        // Then
-        Throwable exception = assertThrows(IllegalArgumentException.class, () ->
-                // When
-                new Chessboard(width, height));
-
-        assertEquals("Wrong Checkerboard size", exception.getMessage());
-    }
 }
