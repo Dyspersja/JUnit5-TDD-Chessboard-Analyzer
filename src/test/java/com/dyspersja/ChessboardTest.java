38602c75332b2c7d25a9cd8189dfd61a61c6c0c5
Dyspersja <dyspersjamusic@gmail.com>
2024-03-16 15:18:21 +0100

    Added tests for correct size

--- src/test/java/com/dyspersja/ChessboardTest.java
+++ src/test/java/com/dyspersja/ChessboardTest.java
@@ -10,10 +10,13 @@ class ChessboardTest {
 
     @ParameterizedTest
     @CsvSource({
+            "-8, -8",   // negative both
             "-2, 8",    // negative width
             "8, -3",    // negative height
+            "1, 0",     // too small both
             "2, 8",     // too small width
             "8, 5",     // too small height
+            "80, 50",   // too big both
             "75, 8",    // too big width
             "8, 45"     // too big height
     })
@@ -26,6 +29,25 @@ class ChessboardTest {
                 new Chessboard(width, height));
 
         assertEquals("Invalid Checkerboard size", exception.getMessage());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "6, 6",     // lowest both
+            "6, 8",     // lowest width
+            "10, 6",    // lowest height
+            "43, 23",   // in the middle
+            "70, 40",   // highest both
+            "70, 6",    // highest width
+            "40, 40"    // highest height
+    })
+    void shouldNotThrowExceptionWhenValidSize(int width, int height) {
+        // Given ...
+
+        // Then
+        assertDoesNotThrow(() ->
+                // When
+                new Chessboard(width, height));
     }
 
 }
