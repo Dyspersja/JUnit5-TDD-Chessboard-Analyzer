efee29c18d8d874e1987fceb3e5f48e7d835abe5
Dyspersja <dyspersjamusic@gmail.com>
2024-03-24 12:48:10 +0100

    Created test class for Tile class

---
+++ src/test/java/com/dyspersja/board/TileTest.java
@@ -0,0 +1,21 @@
+package com.dyspersja.board;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class TileTest {
+
+    @Test
+    void shouldInitializeWithCorrectValues() {
+        // Given ...
+
+        // When
+        Tile tile = new Tile();
+
+        // Then
+        assertEquals(0, tile.getAttackedBy());
+        assertNull(tile.getChessPiece());
+    }
+}
