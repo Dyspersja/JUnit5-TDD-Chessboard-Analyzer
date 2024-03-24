ab8db725f4da8af292f286066233a103be8a3758
Dyspersja <dyspersjamusic@gmail.com>
2024-03-24 18:13:17 +0100

    Added code for calculating attacks by knight

--- src/main/java/com/dyspersja/pieces/Knight.java
+++ src/main/java/com/dyspersja/pieces/Knight.java
@@ -4,13 +4,30 @@ import com.dyspersja.board.Tile;
 
 public class Knight extends ChessPiece {
 
+    private static final int[][] MOVES = {
+            {1, 2}, {-1, 2}, {1, -2}, {-1, -2},
+            {2, 1}, {-2, 1}, {2, -1}, {-2, -1}
+    };
+
     public Knight(int x, int y) {
         super(x, y);
     }
 
     @Override
     public void calculateAttacks(Tile[][] board) {
-        if(!isValidPosition(board[0].length, board.length, x, y))
+        int boardWidth = board[0].length;
+        int boardHeight = board.length;
+
+        if(!isValidPosition(boardWidth, boardHeight, x, y))
             throw new IllegalStateException("Knight is not placed inside the board");
+
+        for (int[] move : MOVES) {
+            int newX = getX() + move[0];
+            int newY = getY() + move[1];
+
+            if (isValidPosition(boardWidth, boardHeight, newX, newY)) {
+                board[newX][newY].incrementAttackedBy();
+            }
+        }
     }
 }
