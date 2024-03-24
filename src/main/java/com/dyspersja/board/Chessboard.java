da83fec351c7aa5628176c41d52cc5e346efdc61
Dyspersja <dyspersjamusic@gmail.com>
2024-03-24 16:34:16 +0100

    Added code for creating board

--- src/main/java/com/dyspersja/board/Chessboard.java
+++ src/main/java/com/dyspersja/board/Chessboard.java
@@ -13,12 +13,30 @@ public class Chessboard {
     private final int width;
     private final int height;
 
+    private final Tile[][] board;
+
     public Chessboard(int width, int height) {
         if( width < MIN_WIDTH || width > MAX_WIDTH || height < MIN_HEIGHT || height > MAX_HEIGHT)
             throw new IllegalArgumentException("Invalid Checkerboard size");
 
         this.width = width;
         this.height = height;
+
+        this.board = initializeBoard(width, height);
+    }
+
+    private Tile[][] initializeBoard(int width, int height){
+        Tile[][] board = new Tile[width][height];
+        for (int column = 0; column < width; column++) {
+            for (int row = 0; row < height; row++) {
+                board[column][row] = new Tile();
+            }
+        }
+        return board;
+    }
+
+    public Tile getTile(int x, int y) {
+        return board[x][y];
     }
 
 }
