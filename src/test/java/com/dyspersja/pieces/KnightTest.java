c06293de616905316f81e57e71ce75d4c3772734
Dyspersja <dyspersjamusic@gmail.com>
2024-03-24 14:05:37 +0100

    Added test for calculateAttacks method

--- src/test/java/com/dyspersja/pieces/KnightTest.java
+++ src/test/java/com/dyspersja/pieces/KnightTest.java
@@ -1,7 +1,9 @@
 package com.dyspersja.pieces;
 
+import com.dyspersja.board.Tile;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class KnightTest {
@@ -20,4 +22,16 @@ public class KnightTest {
         assertEquals(y, knight.getY());
     }
 
+    @Test
+    void shouldThrowExceptionWhenInvalidPosition() {
+        // Given
+        Knight knight = new Knight(20,8);
+        Tile[][] board = new Tile[10][15];
+
+        // When
+        assertThatThrownBy(() -> knight.calculateAttacks(board))
+                // Then
+                .isInstanceOf(IllegalStateException.class)
+                .hasMessage("Knight is not placed inside the board");
+    }
 }
