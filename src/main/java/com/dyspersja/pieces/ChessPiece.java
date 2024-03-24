b04268833503b969feb5ad94773219ecf01337eb
Dyspersja <dyspersjamusic@gmail.com>
2024-03-24 11:23:08 +0100

    Added Knight class

---
+++ src/main/java/com/dyspersja/pieces/ChessPiece.java
@@ -0,0 +1,13 @@
+package com.dyspersja.pieces;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public abstract class ChessPiece {
+
+    protected int x;
+    protected int y;
+
+}
---
+++ src/main/java/com/dyspersja/pieces/Knight.java
@@ -0,0 +1,8 @@
+package com.dyspersja.pieces;
+
+public class Knight extends ChessPiece {
+
+    public Knight(int x, int y) {
+        super(x, y);
+    }
+}
