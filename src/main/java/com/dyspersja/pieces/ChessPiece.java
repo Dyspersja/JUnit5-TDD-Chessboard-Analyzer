36211c9eda391642871e410e312e280c57e57e6b
Dyspersja <dyspersjamusic@gmail.com>
2024-03-24 14:06:03 +0100

    Added calculateAttacks method to ChessPiece and Knight classes

--- src/main/java/com/dyspersja/pieces/ChessPiece.java
+++ src/main/java/com/dyspersja/pieces/ChessPiece.java
@@ -1,5 +1,6 @@
 package com.dyspersja.pieces;
 
+import com.dyspersja.board.Tile;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -10,4 +11,5 @@ public abstract class ChessPiece {
     protected int x;
     protected int y;
 
+    public abstract void calculateAttacks(Tile[][] board);
 }
--- src/main/java/com/dyspersja/pieces/Knight.java
+++ src/main/java/com/dyspersja/pieces/Knight.java
@@ -1,8 +1,16 @@
 package com.dyspersja.pieces;
 
+import com.dyspersja.board.Tile;
+
 public class Knight extends ChessPiece {
 
     public Knight(int x, int y) {
         super(x, y);
+    }
+
+    @Override
+    public void calculateAttacks(Tile[][] board) {
+        if(x >= board.length || y >= board[0].length)
+            throw new IllegalStateException("Knight is not placed inside the board");
     }
 }
