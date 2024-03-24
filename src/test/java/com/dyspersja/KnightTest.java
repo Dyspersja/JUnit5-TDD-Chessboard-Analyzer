fc00b828e395f19d4ecf8ba7986ab5b13e70abf4
Dyspersja <dyspersjamusic@gmail.com>
2024-03-24 11:00:41 +0100

    Added new test class for Knight class

---
+++ src/test/java/com/dyspersja/KnightTest.java
@@ -0,0 +1,23 @@
+package com.dyspersja;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class KnightTest {
+
+    @Test
+    void shouldAssignPositionWhenCreated() {
+        // Given
+        int x = 2;
+        int y = 1;
+
+        // When
+        Knight knight = new Knight(x, y);
+
+        // Then
+        assertEquals(x, knight.getX());
+        assertEquals(y, knight.getY());
+    }
+
+}
