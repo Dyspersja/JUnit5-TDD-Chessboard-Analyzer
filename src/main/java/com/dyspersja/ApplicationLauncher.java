27efec4e15894ee8df94f0a98950a6ecfdc1c383
Dyspersja <dyspersjamusic@gmail.com>
2024-03-24 21:16:51 +0100

    Added Application Launcher

---
+++ src/main/java/com/dyspersja/ApplicationLauncher.java
@@ -0,0 +1,30 @@
+package com.dyspersja;
+
+import com.dyspersja.view.errorscreen.ErrorScreenProvider;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class ApplicationLauncher extends Application {
+
+    @Override
+    public void start(Stage stage) {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/fxml/WelcomeScreen.fxml"));
+            Scene scene = new Scene(fxmlLoader.load());
+            stage.setTitle("Minesweeper");
+            stage.setResizable(false);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IllegalStateException | IOException e) {
+            ErrorScreenProvider.displayErrorWindow(e);
+        }
+    }
+
+    public void launchApplication() {
+        launch();
+    }
+}
--- src/main/java/com/dyspersja/Main.java
+++ src/main/java/com/dyspersja/Main.java
@@ -2,6 +2,7 @@ package com.dyspersja;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var launcher = new ApplicationLauncher();
+        launcher.launchApplication();
     }
 }
